c705ad18b533f4fec6099d38e2c934941cfd6823
Victor Ikuomola <vheecthurikuomola@gmail.com>
2025-04-21 16:35:22 +0100

    feat: adds application entry point

---
+++ cmd/drift-detector/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/sirupsen/logrus"
+	"github.com/victor-devv/ec2-drift-detector/internal/cli"
+	"github.com/victor-devv/ec2-drift-detector/internal/config"
+)
+
+func main() {
+	if err := run(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func run() error {
+	// close context on graceful shutdown
+	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer cancel()
+
+	cfg, err := config.New("")
+	if err != nil {
+		return fmt.Errorf("Failed to load configuration: %v", err)
+	}
+
+	logger := logrus.New()
+	logger.SetFormatter(&logrus.TextFormatter{
+		FullTimestamp: true,
+	})
+	logrus.SetOutput(os.Stdout)
+	logLevel, err := logrus.ParseLevel(cfg.Log.Level)
+	if err != nil {
+		logger.WithError(err).Warn("Invalid log level specified, defaulting to info")
+		logLevel = logrus.InfoLevel
+	}
+	logrus.SetLevel(logLevel)
+
+	return nil
+}
